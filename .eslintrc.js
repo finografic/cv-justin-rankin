001981304ac74e1171aca91e94cc2a3a8fa721fc
Justin <justin.blair.rankin@gmail.com>
2020-05-11 17:01:12 +0200

    working copy of eslint-config-finografic rules imported and recognised

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+const { rules } = require('./eslint-config-finografic');
 module.exports = {
   env: {
     browser: true,
@@ -16,11 +17,13 @@ module.exports = {
     'plugin:i18n-json/recommended',
     // 'plugin:css-modules/recommended',
     'plugin:eslint-comments/recommended',
+    // 'finografic', - INCLUDE WHEN CONFIG IS COMPLETE
   ],
   parser: 'babel-eslint',
   parserOptions: {
     // ecmaVersion: 6,
-    ecmaVersion: 2018,
+    // ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module', // IMPORTANT !!
     ecmaFeatures: {
       jsx: true,
@@ -61,15 +64,13 @@ module.exports = {
   // },
   rules: {
     //'prettier/prettier': ['error'],
-    quotes: ['error', 'single', { avoidEscape: true }],
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'spaced-comment': 1,
-    'no-unused-vars': 2,
     'eslint-comments/no-unused-disable': 'error',
     // [1]   Line 1:1:   Resolve error: unable to load resolver "babel-module"   import/namespace
     // [1]   Line 1:1:   Resolve error: unable to load resolver "babel-module"   import/no-unresolved
     // [1]   Line 1:1:   Resolve error: unable to load resolver "babel-module"   import/named
     // [1]   Line 6:21:  Unable to resolve path to module './components/Testss'  import/no-unresolved
+    ...rules,
   },
 };
---
+++ eslint-config-finografic/eslintrc-rules.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'spaced-comment': 0,
+    'no-unused-vars': 2,
+  },
+};

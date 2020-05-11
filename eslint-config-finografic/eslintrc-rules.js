fd6d22ce407ea35d056313b59a60adeaaba26508
Justin <justin.blair.rankin@gmail.com>
2020-05-11 20:34:02 +0200

    Updates

--- eslint-config-finografic/eslintrc-rules.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rules: {
-    // ...require('./rules/basic').rules,
-    ...require('./rules/test').rules,
-    // quotes: ['error', 'single', { avoidEscape: true }],
-    // 'spaced-comment': 0,
-    // 'no-unused-vars': 2,
-  },
-};
---
+++ precache-manifest.3dfef95f4d4d4ea6a96734c9fd3c4e45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1586ba22073896222e1e76601023f432",
+    "url": "/cv-justin-rankin/index.html"
+  },
+  {
+    "revision": "b271a55fb3d2508223b9",
+    "url": "/cv-justin-rankin/static/css/main.25db0b79.chunk.css"
+  },
+  {
+    "revision": "cf95820a6d0f296961c1",
+    "url": "/cv-justin-rankin/static/js/2.9b267b69.chunk.js"
+  },
+  {
+    "revision": "9afb665a861ae8d954c2ee41a6a2e856",
+    "url": "/cv-justin-rankin/static/js/2.9b267b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b271a55fb3d2508223b9",
+    "url": "/cv-justin-rankin/static/js/main.40da9167.chunk.js"
+  },
+  {
+    "revision": "aa5d859538a612543149",
+    "url": "/cv-justin-rankin/static/js/runtime-main.e392cc14.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/cv-justin-rankin/static/media/logo.be622899.svg"
+  }
+]);

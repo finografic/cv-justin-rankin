4e2bfcdf4362960d66dbed1ba370ec7ef1649128
Justin <justin.blair.rankin@gmail.com>
2020-10-16 12:37:39 +0200

    debugging cleanup + update

--- src/_config/routes.js
+++ src/_config/routes.js
@@ -6,7 +6,6 @@ import { Spinner } from 'components/Spinner';
 // https://react-icons.github.io/react-icons/
 // /react-icon/{used-libary}/index.ems.js
 // /react-icon/{used-libary}/index.ems
-import { FiTriangle, FiGrid } from 'react-icons/fi';
 
 // OPTIONAL PACKAGE TO PREPARE FOR ROUTER v6
 // https://www.npmjs.com/package/react-router-config
@@ -14,30 +13,15 @@ import { FiTriangle, FiGrid } from 'react-icons/fi';
 const loadable = (func) => baseLoadable(func, { fallback: <Spinner /> });
 
 // ROUTES
-const HomePage = loadable(() => import('containers/HomePage'));
-const Dashboard = loadable(() => import('containers/Dashboard'));
-// const CV = loadable(() => import('containers/CV'));
+const CV = loadable(() => import('containers/CV'));
 
 export const routes = [
   {
     path: '/',
     exact: true,
     strict: true,
-    component: HomePage,
-    title: 'Homepage',
-    subtitle: 'Homepage component',
-    icon: <FiTriangle />,
-    isNav: true,
-    key: false,
-  },
-  {
-    path: '/dashboard',
-    exact: true,
-    strict: false,
-    component: Dashboard,
-    title: 'Dashboard',
-    subtitle: 'Dashboard component',
-    icon: <FiGrid />,
+    component: CV,
+    title: 'CV / Curriculum Vitae :: Justin Blair Rankin',
     isNav: true,
     key: false,
   },

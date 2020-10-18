63e8829c5cd70fcde6443256691f00f113a8d1c0
Justin <justin.blair.rankin@gmail.com>
2020-10-18 22:10:17 +0200

    quick fixes for deploying to gh-pages

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,38 +1,40 @@
-import React, { Suspense } from 'react';
-// import { Redirect, Route, Switch } from 'react-router-dom';
-import { Router, Redirect } from '@reach/router';
+// import React, { Suspense } from 'react';
+import React from 'react';
+// import { Router, Redirect } from '@reach/router';
+import { Router } from '@reach/router';
 
 import { Col, Row, Container } from 'react-bootstrap';
 import { useStore } from 'store';
 
 // import { routes } from 'config/routes';
-import { loading, routeLoading } from 'components/Loading';
+// import { loading, routeLoading } from 'components/Loading';
+import { routeLoading } from 'components/Loading';
 
-import SideBar from 'components/Sidebar';
+//import SideBar from 'components/Sidebar';
 import CV from 'containers/CV';
 import { RemoveScroll } from 'react-remove-scroll';
 
-import ButtonPDF from 'components/ButtonPDF';
-import pdf_en from 'assets/CV_Justin-Rankin_EN.pdf';
-import pdf_es from 'assets/CV_Justin-Rankin_ES.pdf';
+// import ButtonPDF from 'components/ButtonPDF';
+// import pdf_en from 'assets/CV_Justin-Rankin_EN.pdf';
+// import pdf_es from 'assets/CV_Justin-Rankin_ES.pdf';
 
-const Footer = React.lazy(() => import('components/Footer'));
-const pdf = {
-  en: pdf_en,
-  es: pdf_es,
-};
+// const Footer = React.lazy(() => import('components/Footer'));
+// const pdf = {
+//   en: pdf_en,
+//   es: pdf_es,
+// };
 
-export default function Layout(props) {
-  const { state, dispatch } = useStore();
+export default function Layout() {
+  const { state } = useStore();
 
   // COOL, BUT NOT REQUIRED FOR THIS PROJECT
   // import useWindowScrollPosition from '@rehooks/window-scroll-position';
   // let position = useWindowScrollPosition();
   // <div className={`app sidebar-${state.sidebar.isOpen} yPos-${position.y}`}>
 
-  const _state = (title) => {
-    state.page.title = title;
-  };
+  // const _state = (title) => {
+  //   state.page.title = title;
+  // };
 
   return (
     <div className={`app sidebar-${state.sidebar.isOpen}`}>
@@ -47,7 +49,7 @@ export default function Layout(props) {
             <Container fluid>
               <Row>
                 <Col style={{ padding: '0px 15px' }}>
-                  <Suspense fallback={routeLoading(state)}>
+                  <React.Suspense fallback={routeLoading(state)}>
                     <Router>
                       <CV path="/" />
                       <CV path="cv-justin-rankin" />
@@ -58,7 +60,7 @@ export default function Layout(props) {
                       <ButtonPDF />
                     </a>
                     */}
-                  </Suspense>
+                  </React.Suspense>
                 </Col>
               </Row>
             </Container>
